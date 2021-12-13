85b1ac0590c6291583a1e7a795f0768054d3038c
airmanxtw <airmanx@stust.edu.tw>
2021-12-13 23:29:38 +0800

    number input

--- test.0.0.1.js
+++ test.0.0.1.js
@@ -1,22 +1,33 @@
 (function ($) {
     $.fn.extend({
-        add:function(parm){ 
+        add: function (parm) {
 
-            let defaultParm={
-                color:"red",
-                oninput:function(){}
+            let defaultParm = {
+                color: "red",
+                oninput: function () { }
             }
-            var newParm = $.extend(defaultParm, parm);   
-            
-            let input=$("<input style='color:red' ></input>");
-            input.css("background-color",newParm.color);
-            input.on('input',function(){  
-                let n0=$(this).val();
-                let n=$.number($(this).val(),0);
+            var newParm = $.extend(defaultParm, parm);
+
+            let input = $("<input></input>");
+            let input2 = $("<input></input>");
+            input.css({
+                "background-color": newParm.color,
+                "text-align": "right",
+                "border": "0px",
+                "border-bottom": "1px solid"
+            });
+            input2.css({
+                "width": 50,
+                "border": "0px",
+                "border-bottom": "1px solid"
+            });
+            input.on('input', function () {
+                let n0 = $(this).val();
+                let n = $.number($(this).val(), 0);
                 console.log(`n0:${n0} n:${n}`);
-                $(this).val(n);            
+                $(this).val(n);
             })
-            $(this).append(input);
-        },        
+            $(this).append(input).append("<span>.</span>").append(input2);
+        },
     });
 })(jQuery);
