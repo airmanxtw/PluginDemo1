08a3c6ecf431760124bf7bd726b438233b9d50d1
airmanxtw <airmanx@stust.edu.tw>
2021-12-19 23:38:27 +0800

    jsview.js

---
+++ cube.0.0.1.js
@@ -0,0 +1,25 @@
+(function ($) {
+    $.fn.extend({
+        cube: function (parm) {
+            let defaultParm = { data: [] };
+            var newParm = $.extend(defaultParm, parm);
+            let css = "width:50px;height: 50px;margin-right:20px;border-width: 1px;border-style:solid;padding:5px"
+            let events = {
+                clickcube: function (index, val) {
+                    $.observable(newParm.data).refresh(
+                        Array.from(newParm.data, function (v, i) {
+                            return i == index ? (val === "O" ? "X" : "O") : v;
+                        })
+                    );
+                }
+            }
+            let temp = `
+                <span style='${css}'  data-link="{on 'click' ~clickcube #index #data}">
+                    {{:#data}}
+                </span>
+            `;
+            let template = $.templates(temp);
+            template.link($(this), newParm.data, events);
+        }
+    });
+})(jQuery);      
